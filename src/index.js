101773f8e7328410ee3e81b6fa88dc66058e882b
wendy <402748215@qq.com>
2018-08-02 10:59:32 +0800

    20180802AssetManagement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Logo from './logo.png';
+import Data from './data/data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // ----Lodash, currently included via a script, is required for this line to work
   // lodash,now imported by script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['你好', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myLogo = new Image();
+  myLogo.src = Logo;
+
+  element.appendChild(myLogo);
+
+  // the Data variable you import it to will contain parsed JSON for easy consumption
+  console.log(Data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
-const path =  require('path');
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    //配置webpack开发服务功能
+    devServer: {
+        host: 'localhost',
+        port: 9000,
+        //服务器代理配置项
+        proxy: {
+            '/help':{
+                target:'http://10.153.29.39:9036',
+                secure:true,
+                changeOrigin:true
+                /*
+                //不显示/help
+                pathRewrite: {
+                    '^/help': ''
+                }
+                */
+            }
+        }
+    }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }, {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            }, {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
+            }, {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            }
+        ]
+    }
 };
